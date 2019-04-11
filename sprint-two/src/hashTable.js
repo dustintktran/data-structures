b6aaedd883e1b7a7045d08e8cb089edb9a5611c8
dustintktran <dustintktran@gmail.com>
2019-04-11 15:12:37 -0700

    Update

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,30 +3,60 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.finders = [];
 };
+
 
 HashTable.prototype.insert = function(finder, value) {
   var index = getIndexBelowMaxForKey(finder, this._limit);
-  if(this[index] !== undefined){
-    for(var i in this._limit){
-      if(this[i]){
-        this.index = value;
-        return;
-      }
-    }
-  }
-  this.index = value;
+  // if(this.index == null){ //if isEmptySlot {set this.index, set finders[index] = finder}
+  //   this.index = value;
+  //   this.finders[index] = finder;
+  // } else { 
+  //   if(this.finderFoundAtIndex(index, finder)){ //if IsntEmpty, but SameFinderUsed replace all values
+  //     this.index = value;
+  //     this.finders[index] = finder;
+  //   } else if(!this.finderFoundAtIndex(index, finder)) { // If IsntEmpty and notTheSameFinder, find a new location
+  //     for(var newLoc = 0; newLoc < this._limit; newLoc++){
+  //       if(this.newLoc == null){  //empty slot found -> add values
+  //         this.newLoc = value;
+  //         this.finders.push(finder); //push to front
+  //       }
+  //     }
+  //   }
+  // }
 };
 
 HashTable.prototype.retrieve = function(finder) {
   var index = getIndexBelowMaxForKey(finder, this._limit);
-  return this.index;
+  //return this._storage.get(index);
+  // if(this.index == null){ //if nothings there return 
+  //   return;
+  // } else { 
+  //   if(this.finderFoundAtIndex(index, finder)){ //if SameFinderUsed return values
+  //    return this.index;
+  //   } else { // If notTheSameFinder, Start search
+  //     for(var newLoc = index; newLoc < this._limit; newLoc++){
+  //       if(this.finderFoundAtIndex(newLoc, finder)){ //ifFinderMatched
+  //         return this.newLoc;
+  //       }
+  //     }
+  //   }
+  // }
+  // return this.index;
 };
 
 HashTable.prototype.remove = function(finder) {
   var index = getIndexBelowMaxForKey(finder, this._limit);
-  delete this.index;  
+
 };
+// //  limitedArray.each = function(callback) {
+//     for (var i = 0; i < storage.length; i++) {
+//       callback(storage[i], i, storage);
+//     }
+//   };
+
+
 
 
 
